32d2805fd7e4a56d6e97ddeb351216b7473977bf
Denis Nikitin <dinya.nikitin@gmail.com>
2022-04-17 21:43:29 +0300

    fix length>50

--- hm_3-2/src/components/Item.js
+++ hm_3-2/src/components/Item.js
@@ -7,16 +7,21 @@ function Item(props) {
     if(!item.title){
             return
     }
-  console.log(item.currency_code);
+  const arr = Array.from(item.title);
+  arr.splice(50);
+  if(arr.length>49){arr.push('...')}
+
+ 
+  console.log(arr.length);
   return (
     <div className="item">
-      <div class="item-image">
+      <div className="item-image">
         <a href={item.url}>
           <img src={item.MainImage.url_570xN} alt='' />
         </a>
       </div>
       <div className="item-details">
-        <p className="item-title">{item.title}</p>
+        <p className="item-title">{arr.join('')}</p>
         <p className="item-price">{item.currency_code==='USA'?'$'+item.price:item.currency_code==='EUR'?'€'+item.price:item.price+'GBR'}</p>
         <p className={`item-quantity level-${item.quantity<11?'low':item.quantity>20?'high':'medium'}`}>{item.quantity}</p>
       </div>
